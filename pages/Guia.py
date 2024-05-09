c2f591f27bb0afec83b7429cbb0906ee4996aced
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 14:24:43 -0400

    Update Guia.py

--- pages/Guia.py
+++ pages/Guia.py
@@ -1,29 +1,56 @@
 import streamlit as st
+import csv
 import pandas as pd
 import os
 import yaml
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 
-# Função para salvar o DataFrame de volta para o arquivo CSV
-def save_dataframe_to_csv(df, csv_file):
-    df.to_csv(csv_file, index=False)
+def save_to_csv(date, name, age, email):
+    # Define CSV file path
+    csv_file = "data.csv"
 
-# Função para carregar o DataFrame do arquivo CSV
-def load_csv_as_dataframe(csv_file):
+    # Write values to CSV file
+    with open(csv_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([date, name, age, email])
+
+def load_csv_as_dataframe():
+    # Define CSV file path
+    csv_file = "data.csv"
+
+    # Read CSV file into a DataFrame
     if os.path.exists(csv_file):
         df = pd.read_csv(csv_file)
     else:
-        df = pd.DataFrame(columns=["Date", "Name", "Age", "Email"])
+        df = pd.DataFrame(columns=["Date", "Name", "Age", "Email"])  # Create empty DataFrame with specified column names
     return df
 
-# Função para limpar o arquivo CSV
-def clear_csv(csv_file):
+def clear_csv():
+    # Define CSV file path
+    csv_file = "data.csv"
+
+    # Check if CSV file exists, then delete it
     if os.path.exists(csv_file):
         os.remove(csv_file)
         st.success("Data cleared successfully!")
     else:
         st.warning("No data to clear.")
+
+def load_items_with_description_from_excel(file_path, column_name_item, column_name_description):
+    # Carrega o arquivo Excel
+    df = pd.read_excel(file_path)
+    
+    # Convertendo os dados das colunas para string e depois concatenando
+    items_with_description = df[column_name_item].astype(str) + " - " + df[column_name_description].astype(str)
+    
+    return items_with_description.tolist()
+
+# Define o caminho do arquivo Excel
+excel_file_path = "Items.xlsx"
+
+# Carrega os itens e descrições do Excel
+items_with_description = load_items_with_description_from_excel(excel_file_path, "Item", "Descrição")
 
 st.sidebar.header("Links Úteis")
 st.sidebar.markdown("[:egg: Rastreabilidade de Ovos](https://esteiraskajoo.streamlit.app/)")
@@ -45,29 +72,29 @@ if authentication_status:
     authenticator.logout('Logout', 'main')
     st.title("Adicionar itens para o guia:")
     
-    # Carregar o arquivo CSV
-    csv_file = "data.csv"
-    df = load_csv_as_dataframe(csv_file)
+    # Create form elements
+    date = st.date_input("Data:", value=None, format="DD/MM/YYYY")
+    name = st.selectbox("Escolher item: ", items_with_description)
+    age = st.number_input("Quantidade:")
+    email = st.text_input("Situação:")
     
-    # Exibir o DataFrame como uma tabela editável
+    if st.button("Enviar"):
+        # Save values to CSV
+        save_to_csv(date, name, age, email)
+        st.success("Dados salvos!")
+    
+    # Load CSV data and display as DataFrame
     st.header("Listagem do Guia de Produção:")
-    for i in range(len(df)):
-        for col in df.columns:
-            if col == "Date":
-                df[col][i] = st.date_input(f"{col} {i}", value=pd.to_datetime(df[col][i]))
-            else:
-                df[col][i] = st.text_input(f"{col} {i}", value=df[col][i])
+    df = load_csv_as_dataframe()
+    #st.dataframe(df)
     
-    # Botão para salvar as alterações
-    if st.button("Salvar Alterações"):
-        save_dataframe_to_csv(df, csv_file)
-        st.success("Alterações salvas com sucesso!")
+    edited_df = st.data_editor(df)
     
-    # Botão para limpar os dados
-    if st.button("Limpar Dados"):
-        clear_csv(csv_file)
-        st.info("Dados excluídos com sucesso!")
+    # Button to clear CSV data
+    if st.button("Limpar dados"):
+        clear_csv()
+        save_to_csv("Date", "Item", "Quantidade", "Situação")
 elif authentication_status == False:
-    st.error('Nome de usuário/senha incorretos')
+    st.error('Username/password is incorrect')
 elif authentication_status == None:
-    st.warning('Por favor, insira seu nome de usuário e senha')
+    st.warning('Please enter your username and password')
