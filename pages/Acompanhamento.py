4033e30e4d69b4f47350548d9354abc43247db00
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 14:57:57 -0400

    Update Acompanhamento.py

--- pages/Acompanhamento.py
+++ pages/Acompanhamento.py
@@ -12,7 +12,7 @@ df["Date"] = pd.to_datetime(df["Date"])
 today = pd.Timestamp.today().date()  # Obtém a data de hoje
 filtered_df = df[df["Date"].dt.date == today]
 
-filtered_df["produzida"] = "Sim"
+filtered_df["A produzir"] = filtered_df["Quantidade"] - filtered_df["Estoque Inicio"]
 
 # Exibe o DataFrame filtrado
 st.write(filtered_df)
