607ef026a1ed1ce813019a3436c3e523cc33fab3
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 12:17:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.page_link("main.py", label="Home", icon="🏠")
-st.page_link("pages/Guia.py", label="Guia de Produção", icon=:clipboard:)
+st.page_link("pages/Guia.py", label="Guia de Produção", icon="📋")
