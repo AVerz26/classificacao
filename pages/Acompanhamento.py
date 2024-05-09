212a72880131039fc4667fb44187996a10ea14b8
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 14:03:45 -0400

    Update Acompanhamento.py

--- pages/Acompanhamento.py
+++ pages/Acompanhamento.py
@@ -12,5 +12,7 @@ df["Date"] = pd.to_datetime(df["Date"])
 today = pd.Timestamp.today().date()  # Obtém a data de hoje
 filtered_df = df[df["Date"].dt.date == today]
 
+filtered_df["produzida"] = "Sim"
+
 # Exibe o DataFrame filtrado
 st.write(filtered_df)
