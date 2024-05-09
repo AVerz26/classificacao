28cbcc0f5c1d3ef99f9d81692c92369eec8a4cff
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 12:22:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.header("Controle de Produção")
+st.title("Controle de Produção")
 
-st.page_link("main.py", label="Home", icon="🏠")
+st.subheader("Páginas disponíveis para consulta:")
+st.page_link("main.py", label="Página de Início", icon="🏠")
+st.write("Página de navegação")
 st.page_link("pages/Guia.py", label="Guia de Produção", icon="📋")
+st.write("Cadastro de itens para o guia de produção")
