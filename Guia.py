9ca4d2e0b36740cbbf84bcc8cdd33a067d57f9cb
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 13:09:27 -0400

    Update Guia.py

--- Guia.py
+++ Guia.py
@@ -2,6 +2,7 @@ import streamlit as st
 import csv
 import pandas as pd
 import os
+import streamlit_authenticator as stauth
 
 def save_to_csv(date, name, age, email):
     # Define CSV file path
@@ -43,36 +44,64 @@ def load_items_with_description_from_excel(file_path, column_name_item, column_n
     
     return items_with_description.tolist()
 
-# Define o caminho do arquivo Excel
-excel_file_path = "Items.xlsx"
+import yaml
+from yaml.loader import SafeLoader
 
-# Carrega os itens e descrições do Excel
-items_with_description = load_items_with_description_from_excel(excel_file_path, "Item", "Descrição")
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-st.sidebar.header("Links Úteis")
-st.sidebar.markdown("[:egg: Rastreabilidade de Ovos](https://esteiraskajoo.streamlit.app/)")
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-st.title("Adicionar itens para o guia:")
+name, authentication_status, username = authenticator.login('Login', 'main')
 
-# Create form elements
-date = st.date_input("Data:", value=None, format="DD/MM/YYYY")
-name = st.selectbox("Escolher item: ", items_with_description)
-age = st.number_input("Quantidade:")
-email = st.text_input("Situação:")
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    # Define o caminho do arquivo Excel
+    excel_file_path = "Items.xlsx"
+    
+    # Carrega os itens e descrições do Excel
+    items_with_description = load_items_with_description_from_excel(excel_file_path, "Item", "Descrição")
+    
+    st.sidebar.header("Links Úteis")
+    st.sidebar.markdown("[:egg: Rastreabilidade de Ovos](https://esteiraskajoo.streamlit.app/)")
+    
+    st.title("Adicionar itens para o guia:")
+    
+    # Create form elements
+    date = st.date_input("Data:", value=None, format="DD/MM/YYYY")
+    name = st.selectbox("Escolher item: ", items_with_description)
+    age = st.number_input("Quantidade:")
+    email = st.text_input("Situação:")
+    
+    if st.button("Enviar"):
+        # Save values to CSV
+        save_to_csv(date, name, age, email)
+        st.success("Dados salvos!")
+    
+    # Load CSV data and display as DataFrame
+    st.header("Listagem do Guia de Produção:")
+    df = load_csv_as_dataframe()
+    #st.dataframe(df)
+    
+    edited_df = st.data_editor(df)
+    
+    # Button to clear CSV data
+    if st.button("Limpar dados"):
+        clear_csv()
+        save_to_csv("Date", "Item", "Quantidade", "Situação")
 
-if st.button("Enviar"):
-    # Save values to CSV
-    save_to_csv(date, name, age, email)
-    st.success("Dados salvos!")
 
-# Load CSV data and display as DataFrame
-st.header("Listagem do Guia de Produção:")
-df = load_csv_as_dataframe()
-#st.dataframe(df)
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
 
-edited_df = st.data_editor(df)
 
-# Button to clear CSV data
-if st.button("Limpar dados"):
-    clear_csv()
-    save_to_csv("Date", "Item", "Quantidade", "Situação")
+
