cfed331cd0c5520a24d97ebb6239035440927d98
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 14:01:38 -0400

    Update Acompanhamento.py

--- pages/Acompanhamento.py
+++ pages/Acompanhamento.py
@@ -6,4 +6,11 @@ csv_file = "data.csv"
 
 df = pd.read_csv(csv_file)
 
-st.write(df)
+df["Date"] = pd.to_datetime(df["Date"])
+
+# Filtra o DataFrame para o dia de hoje
+today = pd.Timestamp.today().date()  # Obtém a data de hoje
+filtered_df = df[df["Date"].dt.date == today]
+
+# Exibe o DataFrame filtrado
+st.write(filtered_df)
