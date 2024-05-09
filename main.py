24f5cef9c618f54dd5dcde35cb2a1533ec56a418
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 12:16:31 -0400

    Update and rename Inicio.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.page_link("Inicio.py", label="Home", icon="🏠")
+st.page_link("main.py", label="Home", icon="🏠")
 st.page_link("pages/Guia.py", label="Guia de Produção", icon=:clipboard:)
