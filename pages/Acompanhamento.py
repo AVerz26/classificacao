6f9cc61d094d4a77b52c27db4cefaff7d40cdbb7
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 15:03:35 -0400

    Update Acompanhamento.py

--- pages/Acompanhamento.py
+++ pages/Acompanhamento.py
@@ -2,19 +2,22 @@ import pandas as pd
 import streamlit as st
 import os
 
+#Importação dos dados
 csv_file = "data.csv"
+excel_file = "BD_PROD.xlsx"
 
+#Dataframes
 df = pd.read_csv(csv_file)
+df2 = pd.read_excel(excel_file)
 
 df["Date"] = pd.to_datetime(df["Date"])
-
 # Filtra o DataFrame para o dia de hoje
 today = pd.Timestamp.today().date()  # Obtém a data de hoje
 filtered_df = df[df["Date"].dt.date == today]
-
 filtered_df["A produzir"] = filtered_df["Quantidade"] - filtered_df["Estoque Inicio"]
 filtered_df = filtered_df.drop(["Quantidade", "Estoque Inicio"], axis = 1)
-
-
 # Exibe o DataFrame filtrado
 st.write(filtered_df)
+
+st.write(df2.head())
+#filtered_df2 = df2[df2["Date"].dt.date == today]
