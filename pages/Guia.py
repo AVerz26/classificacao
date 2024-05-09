2fe0b5b6766138d7b585c3ae48d56c42fc8c6400
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-09 13:22:14 -0400

    Update Guia.py

--- pages/Guia.py
+++ pages/Guia.py
@@ -66,7 +66,7 @@ authenticator = stauth.Authenticate(
     config['preauthorized']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login()
 
 if authentication_status:
     authenticator.logout('Logout', 'main')
