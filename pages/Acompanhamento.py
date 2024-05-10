335c470de4a759097565571a157453cc88f63a00
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-10 12:59:18 -0400

    Update Acompanhamento.py

--- pages/Acompanhamento.py
+++ pages/Acompanhamento.py
@@ -64,7 +64,7 @@ filtered_df.drop(['Descrição'], axis=1, inplace=True)
 
 # ----------------------------------- SITE ----------------------------------- #
 
-st.markdown("<div style='text-align: center;'>Produção ACA: {} caixas </div>"format(int(sum(max(dados_filtrados[column]) for column in dados_filtrados.columns[1:9])/360)), unsafe_allow_html=True)
+#st.markdown("<div style='text-align: center;'>Produção ACA: {} caixas </div>"format(int(sum(max(dados_filtrados[column]) for column in dados_filtrados.columns[1:9])/360)), unsafe_allow_html=True)
 ultimo_valor_data = df2['Dt. Produção'].iloc[-1]
 
 col1, col2 = st.columns(2)
