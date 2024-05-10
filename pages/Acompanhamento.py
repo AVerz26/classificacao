2dec49e9f1158905856ab0232fc8c41c9fe6defa
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-05-10 12:58:19 -0400

    Update Acompanhamento.py

--- pages/Acompanhamento.py
+++ pages/Acompanhamento.py
@@ -62,6 +62,20 @@ nova_ordem_colunas = [colunas[2]] + colunas[:2] + colunas[3:]
 filtered_df = filtered_df[nova_ordem_colunas]
 filtered_df.drop(['Descrição'], axis=1, inplace=True)
 
+# ----------------------------------- SITE ----------------------------------- #
+
+st.markdown("<div style='text-align: center;'>Produção ACA: {} caixas </div>"format(int(sum(max(dados_filtrados[column]) for column in dados_filtrados.columns[1:9])/360)), unsafe_allow_html=True)
+ultimo_valor_data = df2['Dt. Produção'].iloc[-1]
+
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("<div style='text-align: center;'>Produção ACA:  caixas </div>", unsafe_allow_html=True)
+
+# Centralize o conteúdo de col2
+with col2:
+    st.markdown("<div style='text-align: left'><small><em>(Última atualização: {} horas)</em></small></div>".format(ultimo_valor_data), unsafe_allow_html=True)
+
+
 
 st.data_editor(
     filtered_df,
